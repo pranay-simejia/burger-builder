ca55d933e1eb26e9e62d604c48501fc3fac78aed
pranay-simejia <simejiapranay@gmail.com>
2021-02-03 13:34:23 +0530

    Updates

---
+++ precache-manifest.bf92b8b85fe409f6fb5b704c8d04e501.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7eafaba01a156bfbba6e02c3a9b76ea9",
+    "url": "/burger-builder/index.html"
+  },
+  {
+    "revision": "1a626fd58554e97b563c",
+    "url": "/burger-builder/static/css/main.ffd6477c.chunk.css"
+  },
+  {
+    "revision": "97ab13d20410bd5049b7",
+    "url": "/burger-builder/static/js/2.eeba5fa0.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/burger-builder/static/js/2.eeba5fa0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1a626fd58554e97b563c",
+    "url": "/burger-builder/static/js/main.199432c7.chunk.js"
+  },
+  {
+    "revision": "3743a9caf0853cc6f96a",
+    "url": "/burger-builder/static/js/runtime-main.f3cd13ce.js"
+  },
+  {
+    "revision": "b8503d262bffbfb7c67fd6762963e7d1",
+    "url": "/burger-builder/static/media/burger-logo.b8503d26.png"
+  }
+]);
